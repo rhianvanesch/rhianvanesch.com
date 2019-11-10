c29b1d0b89bf39bceeb5bc74e5cf7e37cdcce969
Rhian van Esch <rhian.vanesch@gmail.com>
2019-11-10 21:28:36 +0100

    feat(rss): add RSS feed using eleventy-plugin-rss

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  author: {
+    name: "Rhian van Esch"
+  },
   buildTime: new Date(),
-  environment: process.env.ELEVENTY_ENV
+  environment: process.env.ELEVENTY_ENV,
+  feed: {
+    filename: "feed.xml",
+    id: "https://rhianvanesch.com/",
+    path: "/feed.xml",
+    url: "https://rhianvanesch.com/feed.xml"
+  },
+  title: "Rhian van Esch",
+  url: "https://rhianvanesch.com/"
 };
