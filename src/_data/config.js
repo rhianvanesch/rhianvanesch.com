0c983e9a0a0a61d2934951177f5e61e94e46a930
Rhian van Esch <rhian.vanesch@gmail.com>
2019-11-11 19:13:39 +0100

    feat(layouts): add OpenGraph and Twitter meta tags

--- src/_data/config.js
+++ src/_data/config.js
@@ -3,13 +3,14 @@ module.exports = {
     name: "Rhian van Esch"
   },
   buildTime: new Date(),
+  description: "The personal site of Rhian van Esch, front-end developer.",
   environment: process.env.ELEVENTY_ENV,
   feed: {
     filename: "feed.xml",
-    id: "https://rhianvanesch.com/",
+    id: "https://rhianvanesch.com",
     path: "/feed.xml",
     url: "https://rhianvanesch.com/feed.xml"
   },
   title: "Rhian van Esch",
-  url: "https://rhianvanesch.com/"
+  url: "https://rhianvanesch.com"
 };
