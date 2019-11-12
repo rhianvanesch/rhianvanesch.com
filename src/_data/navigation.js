c09eec819b19ecd379e3434b9292b611a08f0260
Rhian van Esch <rhian.vanesch@gmail.com>
2019-11-12 20:45:26 +0100

    fix(data): add uses to navigation data file

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,17 +1,13 @@
 module.exports = {
   items: [
-    // {
-    //   text: "About",
-    //   url: "/about"
-    // },
     {
       text: "Posts",
       url: "/posts"
     },
-    // {
-    //   text: "Uses",
-    //   url: "/uses"
-    // },
+    {
+      text: "Uses",
+      url: "/uses"
+    },
     {
       text: "Contact",
       url: "/contact"
