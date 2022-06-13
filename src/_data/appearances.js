07bb672cd057234440082b10e49751a479179983
Rhian van Esch <rhian.vanesch@gmail.com>
2022-06-13 14:53:54 +0200

    feat: add appearances section

---
+++ src/_data/appearances.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    date: "2022-06-05",
+    location: "Virtual",
+    on: "Front End Happy Hour",
+    title: "Building in 2022 and tasting the beverage trends",
+    type: "Podcast",
+    url: "https://www.frontendhappyhour.com/episodes/building-in-2022-and-tasting-the-beverage-trends",
+  },
+  {
+    date: "2022-01-13",
+    location: "Virtual",
+    on: "Sendcloud Engineering meetup",
+    title: "UX and DX in 2022",
+    type: "Talk",
+    url: "https://www.youtube.com/watch?v=JzDVIp_kffI",
+  },
+]
