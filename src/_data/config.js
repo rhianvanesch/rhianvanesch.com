26100aace8a589fd2ce24c257a99fea3093cbfab
Rhian van Esch <rhian.vanesch@gmail.com>
2022-06-13 20:50:08 +0200

    chore(deps): update eleventy, remove unused deps
    - update eleventy and plugins to latest versions
    - remove rfs
    - remove cross-env and references

--- src/_data/config.js
+++ src/_data/config.js
@@ -1,16 +1,15 @@
 module.exports = {
   author: {
-    name: "Rhian van Esch"
+    name: "Rhian van Esch",
   },
   buildTime: new Date(),
   description: "The personal site of Rhian van Esch, front-end developer.",
-  environment: process.env.ELEVENTY_ENV,
   feed: {
     filename: "feed.xml",
     id: "https://rhianvanesch.com",
     path: "/feed.xml",
-    url: "https://rhianvanesch.com/feed.xml"
+    url: "https://rhianvanesch.com/feed.xml",
   },
   title: "Rhian van Esch",
-  url: "https://rhianvanesch.com"
-};
+  url: "https://rhianvanesch.com",
+}
