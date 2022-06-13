e1eda52b8c489ed89cc8605777e88614828f04b8
Rhian van Esch <rhian.vanesch@gmail.com>
2022-06-13 15:13:29 +0200

    fix: add datetime attribute to all <time> elements

--- src/filters/date.js
+++ src/filters/date.js
@@ -1,12 +1,12 @@
 // Stolen from https://stackoverflow.com/a/31615643
-const appendSuffix = n => {
+const appendSuffix = (n) => {
   var s = ["th", "st", "nd", "rd"],
-    v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-};
+    v = n % 100
+  return n + (s[(v - 20) % 10] || s[v] || s[0])
+}
 
 function toFullDate(value) {
-  const dateObject = new Date(value);
+  const dateObject = new Date(value)
 
   const months = [
     "January",
@@ -20,17 +20,17 @@ function toFullDate(value) {
     "September",
     "October",
     "November",
-    "December"
-  ];
-  const dayWithSuffix = appendSuffix(dateObject.getDate());
+    "December",
+  ]
+  const dayWithSuffix = appendSuffix(dateObject.getDate())
 
   return `${dayWithSuffix} ${
     months[dateObject.getMonth()]
-  } ${dateObject.getFullYear()}`;
+  } ${dateObject.getFullYear()}`
 }
 
 function getDayMonth(value) {
-  const dateObject = new Date(value);
+  const dateObject = new Date(value)
 
   const months = [
     "Jan",
@@ -44,19 +44,24 @@ function getDayMonth(value) {
     "Sept",
     "Oct",
     "Nov",
-    "Dec"
-  ];
+    "Dec",
+  ]
 
-  return `${months[dateObject.getMonth()]} ${dateObject.getDate()}`;
+  return `${months[dateObject.getMonth()]} ${dateObject.getDate()}`
 }
 
 function getYear(value) {
-  const dateObject = new Date(value);
-  return dateObject.getFullYear();
+  const dateObject = new Date(value)
+  return dateObject.getFullYear()
+}
+
+function getDatetime(value) {
+  return new Date(value).toISOString().split("T")[0]
 }
 
 module.exports = {
+  getDatetime,
   getDayMonth,
   getYear,
-  toFullDate
-};
+  toFullDate,
+}
